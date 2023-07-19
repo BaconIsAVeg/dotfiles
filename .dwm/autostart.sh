6e3955a4395ae0cc4ecea8c22f8faa78587bff11
BaconIsAVeg <arathsin2019@gmail.com>
2023-07-19 01:48:04 -0600

    .

---
+++ .dwm/autostart.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+pgrep -x openrgb > /dev/null || openrgb --server &
+pgrep -x streamdeck > /dev/null || streamdeck -n &
+picom -b &
+
+/usr/bin/dunst &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+if [ -x ~/.fehbg ]; then
+  ~/.fehbg
+else
+  feh --no-fehbg --bg-fill --no-xinerama ${HOME}/.config/wallpaper.jpg
+fi
+
+xautolock -locker slock -nowlocker slock -time 30 -secure &
