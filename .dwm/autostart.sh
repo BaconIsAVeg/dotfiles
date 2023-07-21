ba3f4d76269ad6da8709b06dc2d1cc3424eb65e7
BaconIsAVeg <arathsin2019@gmail.com>
2023-07-21 06:55:05 -0600

    Finally got polybar and dwm built to work together

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,16 +2,13 @@
 
 pgrep -x openrgb > /dev/null || openrgb --server &
 pgrep -x streamdeck > /dev/null || streamdeck -n &
-picom -b &
-
-/usr/bin/dunst &
-/usr/bin/dwmblocks &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+pgrep -x mpd > /dev/null || /usr/bin/mpd ${HOME}/.config/mpd/mpd.conf &
+pgrep polkit-gnome > /dev/null || /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+pgrep -x picom > /dev/null || picom -b &
+pgrep -x dunst > /dev/null || dunst &
 
 if [ -x ~/.fehbg ]; then
   ~/.fehbg
 else
   feh --no-fehbg --bg-fill --no-xinerama ${HOME}/.config/wallpaper.jpg
 fi
-
-xautolock -locker slock -nowlocker slock -time 30 -secure &
