52a2a465a5f1cd866689a53d84e363305a8aa522
BaconIsAVeg <arathsin2019@gmail.com>
2023-08-06 07:23:09 -0600

    .

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -4,26 +4,54 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 vim.opt.timeoutlen = 250 -- or 500 (Default: 1000)
-lvim.colorscheme = "github_dark_tritanopia"
+vim.g.transparent_background = true
+vim.g.gitblame_enabled = 0
 
 lvim.plugins = {
   {
     "dhruvasagar/vim-table-mode"
   },
   {
-    "projekt0n/github-nvim-theme",
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
     config = function()
-      require("github-theme").setup({
-        themeStyle = "dimmed",
-        commentStyle = "NONE",
-        keywordStyle = "NONE",
-        functionStyle = "NONE",
-        variableStyle = "NONE",
-        options = {
-          darken = {
-            sidebars = { "qf", "vista_kind", "terminal", "packer" },
-          }
-        }
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "storm",  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark",        -- style for sidebars, see below
+          floats = "dark",          -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3,       -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false,       -- dims inactive windows
+        lualine_bold = false,       -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors)
+        end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
       })
     end,
   },
@@ -73,5 +101,3 @@ lvim.builtin.which_key.mappings["t"] = {
 }
 
 lvim.builtin.which_key.mappings["s"]["m"] = { "<cmd>Telescope marks<cr>", "Search Marks" }
-
-vim.g.gitblame_enabled = 0
