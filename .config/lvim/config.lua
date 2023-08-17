fc1ffe4683c6fd99aa3711d5d65bd8030b124913
BaconIsAVeg <arathsin2019@gmail.com>
2023-08-16 23:58:40 -0600

    .

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -6,11 +6,17 @@
 vim.opt.timeoutlen = 250 -- or 500 (Default: 1000)
 vim.g.transparent_background = true
 vim.g.gitblame_enabled = 0
-lvim.colorscheme = "tokyonight-storm"
+lvim.colorscheme = "dracula"
 
 lvim.plugins = {
   {
     "dhruvasagar/vim-table-mode"
+  },
+  {
+    "dracula/vim"
+  },
+  {
+    "chaimleib/vim-renpy"
   },
   {
     "folke/tokyonight.nvim",
@@ -102,3 +108,12 @@ lvim.builtin.which_key.mappings["t"] = {
 }
 
 lvim.builtin.which_key.mappings["s"]["m"] = { "<cmd>Telescope marks<cr>", "Search Marks" }
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
