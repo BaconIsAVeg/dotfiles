30c218cf0f81012b5c2bd3b7266cb5a02dac4efa
BaconIsAVeg <arathsin2019@gmail.com>
2023-08-24 12:37:30 -0600

    Auto commit

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,15 @@
-config.load_autoconfig()
-config.set('tabs.show', 'switching')
+import dracula.draw
 
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+default_editor = ["kitty", "-e", "lvim", "-f", "{file}"]
+
+config.set('editor.command', default_editor)
 config.bind('<Ctrl-Shift-l>', 'spawn --userscript qute-bitwarden')
